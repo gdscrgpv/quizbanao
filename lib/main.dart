5b2fbbe98a2f6c0fa5eedec9935dd7e779e10677
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-10-27 18:17:21 +0530

    Squashed commit of the following:
    commit 1051225b664a348ee72aa39bf741f899927dd340
    Author: Ankur Gupta <ankurg052@gmail.com>
    Date:   Wed Oct 27 00:03:56 2021 +0530
    
        firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quizbanao/screens/login.dart';
 import 'package:quizbanao/screens/questions.dart';
 import 'package:quizbanao/screens/results.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
